bf4de27beeee63531f45891d64b583c856d5ba57
mostlyClicks <carlos@mostlyclicks.com>
2017-11-01 13:45:40 -0600

    add ruby 1.9.1-dev

--- provision.sh
+++ provision.sh
@@ -15,6 +15,7 @@ apt-get -y install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
 apt-get -y install imagemagick
 apt-get -y install nodejs
 apt-get -y install postgresql-9.4
+apt-get -y install ruby1.9.1-dev
 
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
